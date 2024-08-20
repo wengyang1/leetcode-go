247217b9cc54e90ad48f549a550f2d93d1a8c33b
longya <longya1@jushuitan.com>
2024-08-20 22:33:56 +0800

    1

--- 000015.py
+++ 000015.py
@@ -7,11 +7,16 @@ class Solution(object):
         # 双指针法
         left = start
         right = end
+        last_valid_second = float('inf')
         while left < right:
             second = nums[left]
             third = nums[right]
             if second + third + target == 0:
-                two_result.append([target, second, third])
+                # 排除重复答案，如果本次的第二个元素和上次的第二个元素相同，
+                # 那么就是重复答案，双指针继续向内靠拢
+                if not last_valid_second == second:
+                    two_result.append([target, second, third])
+                    last_valid_second = second
                 left += 1
                 right -= 1
             elif second + third + target > 0:
@@ -28,6 +33,9 @@ class Solution(object):
         n = len(nums)
         # n-2是因为至少要给后面留2个元素
         for first in range(n - 2):
+            # 不可以包含重复的三元组，所以答案中的第一个元素不能重复
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
             # 遍历first后面的数组片段找剩余的second和third
             two_result = self.twoSum(nums, nums[first], first + 1, n - 1)
             for i in two_result:
